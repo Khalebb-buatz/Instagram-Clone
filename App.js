3ad1bffe41b98cbf3d849661a4f14509be87e630
Khalebb-buatz <khalebbbuatz03@gmail.com>
2021-02-17 18:18:12 +0000

    installing react-native navigation

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={{flex:1,justifyContent:'center',alignItems:'center'}}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Open up App.js to start working on your App!
+      </Text>
       
     </View>
   );
